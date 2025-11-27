c3bdc940bce50cdfe92f5098a964ae0a40804e11
Amos Wenger <amos@bearcove.eu>
2025-11-27 20:13:45 +0100

    Merged into main facet monorepo
    This crate has been merged into https://github.com/facet-rs/facet
    and will continue to be published from there.

--- examples/vec_u8.rs
+++
@@ -1,8 +0,0 @@
-use facet_pretty::FacetPretty;
-
-fn main() {
-    let mut file = std::fs::File::open("/dev/urandom").expect("Failed to open /dev/urandom");
-    let mut bytes = vec![0u8; 128];
-    std::io::Read::read_exact(&mut file, &mut bytes).expect("Failed to read from /dev/urandom");
-    println!("{}", bytes.pretty());
-}
